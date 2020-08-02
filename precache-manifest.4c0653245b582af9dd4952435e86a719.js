16b8115c598a9c64a5f0ccd69373ea72c0a8c9e8
Dominic Cayme <dcayme@gmail.com>
2020-08-01 19:27:30 -0700

    Updates

---
+++ precache-manifest.4c0653245b582af9dd4952435e86a719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2fef9bcf45245de4bd42fb0bb75c91",
+    "url": "/reacttest4/index.html"
+  },
+  {
+    "revision": "e6c0f8f0194d10d7590c",
+    "url": "/reacttest4/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea93a409d7e0e787338b",
+    "url": "/reacttest4/static/js/2.ff528cf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttest4/static/js/2.ff528cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c0f8f0194d10d7590c",
+    "url": "/reacttest4/static/js/main.5db3c248.chunk.js"
+  },
+  {
+    "revision": "f30c05ac54e2c9bb7d4a",
+    "url": "/reacttest4/static/js/runtime-main.84e74bb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttest4/static/media/logo.5d5d9eef.svg"
+  }
+]);
